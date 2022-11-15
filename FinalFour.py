4ad837ee6a69e44d0b592df9e58bd96c2f4de15b
conklin19 <conklin19@gmail.com>
2022-11-15 01:38:58 -0500

    read in csv from assets

--- FinalFour.py
+++ FinalFour.py
@@ -7,3 +7,8 @@ import matplotlib as mp
 import pandas as pd
 import seaborn as sns
 import numpy as np
+
+##Requirement 1.2 Read-In local csv using pandas ###
+df = pd.read_csv('assets\KPdata.csv')
+
+FFdata = df  #create a copy of the dataframe for testing#
